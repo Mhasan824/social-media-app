ada827a00c381c6cb1ab66972a3af15f4dccd0ad
Mhasan824 <alishbataha0@gmail.com>
2026-04-18 12:43:25 -0700

    removed hardcoded localhost

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(cookieparser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://social-media-app-frontend.vercel.app",
   credentials: true
 }));
 
